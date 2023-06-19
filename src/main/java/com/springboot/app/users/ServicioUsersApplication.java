cefd678be6de05ae67e6d5dbef38c0f2fe258033
Richard <richard.fisi@gmail.com>
2023-06-19 01:09:44 -0500

    iniciando servicio users

---
+++ src/main/java/com/springboot/app/users/ServicioUsersApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.app.users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicioUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicioUsersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/app/users/ServicioUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.app.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
