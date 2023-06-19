98f2b10f30f1fd8ab92eb56e1f9d0f6a33d408c9
Richard <richard.fisi@gmail.com>
2023-06-19 06:49:28 -0500

    creacion de las apis crud para user y post by user.

---
+++ src/main/java/com/springboot/app/users/models/entity/Post.java
@@ -0,0 +1,96 @@
+package com.springboot.app.users.models.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.JoinColumn;
+
+@Entity
+@Table(name = "posts")
+public class Post implements Serializable {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String text;
+
+	@ManyToOne(fetch = FetchType.LAZY, optional = false)
+	@JoinColumn(name = "tutorial_id", nullable = false)
+	@OnDelete(action = OnDeleteAction.CASCADE)
+	@JsonIgnore
+	private User user;
+
+	@Column(name = "create_at")
+	@Temporal(TemporalType.DATE)
+	@CreationTimestamp
+	@JsonFormat(pattern="yyyy-MM-dd")
+	private Date createAt;
+
+	@Column(name = "modified_at")
+	@Temporal(TemporalType.TIMESTAMP)
+	@UpdateTimestamp
+	@JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
+	private Date modifiedAt;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public Date getCreateAt() {
+		return createAt;
+	}
+
+	public void setCreateAt(Date createAt) {
+		this.createAt = createAt;
+	}
+
+	public Date getModifiedAt() {
+		return modifiedAt;
+	}
+
+	public void setModifiedAt(Date modifiedAt) {
+		this.modifiedAt = modifiedAt;
+	}
+
+	private static final long serialVersionUID = 8865445792031764203L;
+
+}
